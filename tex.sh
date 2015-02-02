0638024cd5285d97925d2a7e660f01e7b7f4db67
Atsuhiko <Atsuhiko@murakamiatsuhiko-no-MacBook-Pro.local>
2015-02-02 22:14:17 +0900

    2/2

--- tex.sh
+++ tex.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 #author: Atsuhiko Murakami
 
-platex ./report.tex
+pbibtex report
 if [ $? -eq 0 ]
 then
-    dvipdfmx ./report.dvi
+    platex ./report.tex
     if [ $? -eq 0 ]
     then
-	open report.pdf
+	dvipdfmx ./report.dvi
+	if [ $? -eq 0 ]
+	then
+	    open report.pdf
+	fi
     fi
 fi
-     
 
